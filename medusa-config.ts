a1bc85db6727e7ad770a2a8ec6237ee1bdaa0e09
Raj Mahil <webtest10111011@gmail.com>
2025-09-15 18:28:07 -0600

    update

--- medusa-config.ts
+++ medusa-config.ts
@@ -12,6 +12,11 @@ module.exports = defineConfig({
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     },
+    databaseDriverOptions: {
+      connection: { ssl: { rejectUnauthorized: false } },
+      ssl: false,
+    },
+
     workerMode: process.env.MEDUSA_WORKER_MODE as
       | "shared"
       | "worker"
